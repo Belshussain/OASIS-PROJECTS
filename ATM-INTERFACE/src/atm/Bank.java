89bf1122dd732291b0de63689a9d5e7c5034f18a
Belshussain <125981596+Belshussain@users.noreply.github.com>
2024-07-14 20:02:34 +0200

    Bank.java

--- ATM-INTERFACE/src/atm/Bank.java
+++ ATM-INTERFACE/src/atm/Bank.java
@@ -4,15 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Bank {
-	private Map<String, User> users;
-	
-	public Bank() {
-		users = new HashMap<>(); 
-		users.put("user00", new User("user00", "1111"));
-	}
-	
-	public User getUser(String userID) {
-		return users.get(userID);
-	}
-
+    private Map<String, User> users;
+    
+    public Bank() {
+        users = new HashMap<>(); 
+        users.put("user00", new User("user00", "1111"));
+        users.put("user02", new User("user02", "2222")); //recipient
+    }
+    
+    public User getUser(String userID) {
+        return users.get(userID);
+    }
 }
